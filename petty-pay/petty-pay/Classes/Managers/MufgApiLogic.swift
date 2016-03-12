0134f9dbd9c817d584690282269402b29c169b94
uryyyyyyy <koki305@gmail.com>
2016-03-13 02:47:20 +0900

    add

---
+++ petty-pay/petty-pay/Classes/Managers/MufgApiLogic.swift
@@ -0,0 +1,13 @@
+//
+//  MufgApiLogic.swift
+//  petty-pay
+//
+//  Created by Koki Shibata on 3/13/16.
+//  Copyright © 2016 Petty Pay Team. All rights reserved.
+//
+
+import UIKit
+
+class MufgApiLogic: NSObject {
+
+}
